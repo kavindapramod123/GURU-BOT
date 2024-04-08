7910f79b5ed95495cdc24ff5f43113f4d032a752
kavindapramod123 <160144762+kavindapramod123@users.noreply.github.com>
2024-04-08 09:38:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94720602257', 'STAUS KING VIMU', true],
+  ['94720602257', 'STAUS KING VIMU', true], 
   [''] 
 ] //Number of owners
 
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.botname = 'STAUS KING VIMU'
+global.premium = 'false'
+global.packname = 'STAUS KING VIMU' 
+global.author = 'STAUS KING VIMU' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
